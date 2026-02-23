fdf2fa983ef47eab28911c5ad9c96b669fb2e26d
Bernard Hilario <bernardhilario142005@gmail.com>
2026-02-23 13:38:29 +0800

    Wrap Lanyard component in a styled div

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,25 @@ import Lanyard from './component/Lanyard';
 
 function App() {
   return (
-    <>
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        overflow: 'hidden',
+        position: 'relative',
+        backgroundColor: '#000', // Optional: dark background
+      }}
+    >
       <Lanyard
         position={[0, 0, 20]}
         gravity={[0, -40, 0]}
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'block',
+        }}
       />
-    </>
+    </div>
   );
 }
 
